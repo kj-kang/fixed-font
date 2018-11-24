3462920ec8f0c21ed830b87a00700a325f8b76d1
jayden-kang <jayden.kang@kakaocorp.com>
2018-11-24 18:13:02 +0900

    add README.md and refactoring

---
+++ README.md
@@ -0,0 +1,62 @@
+## 개요
+
+글꼴의 경우 글자폭의 형태에 따라 가변폭, 고정폭으로 구분한다. 가변폭의
+경우 "W" 와 "I" 의 가로폭(width)이 다르며, 고정폭은 "I" 의 폭을 "W" 에
+맞추어 개발한 글꼴이다. 일반적으로 가변폭 형태의 글꼴이 읽이게
+편안하고 가독성이 높다. 그러나 숫자, 기호, 띄어쓰기 등을 명확하게
+표현하기가 어렵다.
+
+같은 고정폭이라 하더라도 영문과 한글 글꼴을 같이 사용하는 경우 폭이
+달라질 수 있다. 아래와 같이 텍스트로 테이블을 만드는 경우 그 차이를
+확인할 수 있다.
+
+| 한글 | 테스트 |
+| ABCD | abcdef |
+
+이 패키지는 Eamcs에서 영문과 한글 글꼴을 같은 고정폭으로 사용할 수
+있도록 한다. 한글 글꼴을 D2Coding 을 기본으로 하고 영문은 다음과 같은
+글꼴을 테스트하였다.
+
+## 설치
+
+`fixed-font.el` 파일을 `~/.emacs.d/site-lisp` 디렉토리에
+다운로드한다. 아래와 같이 `.emacs` (or `init.el`) 파일에 추가한다.
+
+```lisp
+(use-package fixed-font
+  :load-path "~/.emacs.d/site-lisp/fixed-font"
+  :config
+  (fixed-font-set-height-default))
+```
+
+## 설정
+
+기본 영문, 한글 글꼴과 크기는 다음과 같이 지정한다.
+
+```lisp
+;; 영문 글꼴을 지정한다.
+(setq fixed-font-ascii-font "Monaco")
+
+;; 한글 글꼴을 지정한다.
+(setq fixed-font-non-ascii-font "D2Coding")
+
+;; 기본크기를 지정한다.
+(setq fixed-font-default-height 130)
+```
+
+`use-pacakge` 내의 `bind` 설정을 통해 아래와 같이 단축키를 지정할 수
+있다.
+
+```
+(use-package fixed-font
+  :load-path "~/.emacs.d/site-lisp/fixed-font"
+  :bind
+  ;; 기본 글꼴, 크기로 변경한다.
+  ("s-0" . fixed-font-set-height-default)
+
+  ;; 글꼴을 한단계 증가시킨다.
+  ("s-=" . fixed-font-increase-height)
+
+  ;; 글꼴을 한단계 감소시킨다.
+  ("s--" . fixed-font-decrease-height))
+```
--- fixed-font.el
+++ fixed-font.el
@@ -6,53 +6,83 @@
 ;;; Code:
 ;;;
 
-
 (require 'seq)
 
 
-(defcustom fixed-font-ascii-font "Envy Code R"
+(defcustom fixed-font-ascii-font "Monaco"
   "영문 글꼴의 이름을 지정한다."
   :type  'string
-  :tag   'fixed-font
   :group 'fixed-font)
 
 
 (defcustom fixed-font-non-ascii-font "D2Coding"
   "한글 글꼴의 이름을 지정한다."
   :type  'string
-  :tag   'fixed-font
   :group 'fixed-font)
 
 
 (defcustom fixed-font-default-height 160
   "글꼴의 기본 크기를 지정한다."
   :type  'int
-  :tag   'fixed-font
   :group 'fixed-font)
 
 
 (defvar fixed-font-rescale-alist
-  '((100 . 1.05)
-    (110 . 1.10)
-    (120 . 1.05)
-    (130 . 1.10)
+  '(("DejaVu Sans Mono" .
+     ((100 . 1.25)
+      (110 . 1.30)
+      (120 . 1.20)
+      (130 . 1.25)
+      (140 . 1.15)
+      (150 . 1.20)
+      (160 . 1.25)
+      (170 . 1.20)
+      (180 . 1.25)
+      (190 . 1.20)
+      (200 . 1.20)))
+    ("Envy Code R" .
+     ((110 . 1.10)
+      (120 . 1.05)
+      (130 . 1.10)
+      (140 . 1.15)
+      (150 . 1.10)
+      (160 . 1.15)
+      (170 . 1.10)
+      (180 . 1.15)
+      (190 . 1.10)
+      (200 . 1.10)))))
+
+
+(defvar fixed-font-default-rescale-alist
+  '((100 . 1.20)
+    (110 . 1.30)
+    (120 . 1.20)
+    (130 . 1.25)
     (140 . 1.15)
-    (150 . 1.10)
-    (160 . 1.15)
-    (170 . 1.10)
-    (180 . 1.15)
-    (190 . 1.10)
-    (200 . 1.10)))
+    (150 . 1.20)
+    (160 . 1.25)
+    (170 . 1.20)
+    (180 . 1.25)
+    (190 . 1.20)
+    (200 . 1.20)))
 
 
-(defun fixed-font--get-rescale (height)
-  "영문 글꼴의 크기(HEIGHT)에 따른 한글 글꼴의 스케일을 반환한다."
-  (cdr (car (last (seq-take-while (lambda (x) (<= (car x) height)) fixed-font-rescale-alist)))))
+(defun fixed-font--get-rescale (ascii-font height)
+  "영문 글꼴(ASCII-FONT)과 높이(HEIGHT)에 맞는 스케일을 반환한다.
+만약 해당하는 스케일을 찾을 수 없다면 fixed-font-default-rescale-alist
+  의 값을 참조하여 반환한다."
+  (let* ((rescale-alist (assoc ascii-font fixed-font-rescale-alist))
+	 (rescale       (assoc height rescale-alist)))
+    (cdr (if (eq rescale-alist nil)
+	(assoc height fixed-font-default-rescale-alist)
+	rescale))))
 
 
 (defun fixed-font--set-height (height)
-  "글꼴의 크기(HEIGHT)를 설정한다."
-  (let ((rescale (fixed-font--get-rescale height)))
+  "글꼴의 높이(HEIGHT)를 설정한다."
+  (let ((rescale (fixed-font--get-rescale fixed-font-ascii-font
+					  height)))
+    (message "font: %s, height: %d, scale: %.2f" fixed-font-ascii-font height rescale)
     (set-face-attribute 'default nil :family fixed-font-ascii-font)
     (set-fontset-font t 'hangul (font-spec :family fixed-font-non-ascii-font))
     (setq face-font-rescale-alist `((,fixed-font-non-ascii-font . ,rescale)))
@@ -61,23 +91,26 @@
 
 ;;;###autoload
 (defun fixed-font-set-height-default ()
-  "글꼴의 크기를 기본 크기로 지정한다."
+  "글꼴의 높이를 기본 크기로 지정한다."
   (interactive)
   (fixed-font--set-height fixed-font-default-height))
 
 
 ;;;###autoload
 (defun fixed-font-increase-height ()
-  "글꼴의 크기를 한단계(10) 높인다."
+  "글꼴의 높이를 한단계(10) 높인다."
   (interactive)
   (let ((height (+ (face-attribute 'default :height) 10)))
     (fixed-font--set-height height)))
 
+
+;;;###autoload
 (defun fixed-font-decrease-height ()
-  "글꼴의 크기를 한단계(10) 줄인다."
+  "글꼴의 높이를 한단계(10) 줄인다."
   (interactive)
   (let ((height (- (face-attribute 'default :height) 10)))
     (fixed-font--set-height height)))
+
 
 (provide 'fixed-font)
 
